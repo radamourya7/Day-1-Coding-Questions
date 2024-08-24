8406ffdab96855914bb7571873d0e00c48aa1380
Mourya Rada <145229013+radamourya7@users.noreply.github.com>
2024-08-25 01:27:41 +0530

    Update Q3.cpp
    Write a C++ program that takes an integer as input from the user and outputs its square.

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,19 @@
+Ques- 03 Write a C++ program that takes an integer as input from the user and outputs its square.
 
+Sample Input:- Enter a number: 7
+
+Sample Output :- The square of 7 is: 49
+
+
+CODE:=
+  
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int square=n*n;
+    cout<<"The square of "<<n<<" is: "<<square;
+    return 0;
+}
